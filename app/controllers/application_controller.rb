ca1594d298906619d4402bc05546847428f7f9f2
Grafiters <bayugrafit@gmail.com>
2023-01-04 07:58:51 +0700

    feat(minor): split function to handle request

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,19 @@
 class ApplicationController < ActionController::API
+    private
+    def find_email_user
+        user = User.find_by(email: params[:email])
+        
+        user.nil? ? "" : render_duplicate(params[:email])
+    end
+
+    def insert_to_mailer
+        mailer = Mailer.create(mailer_user_param[0])
+        VerifyAccountMailer.with(email: params[:email], pin: mailer[:pin]).confirmation.deliver_now
+    end
+
+    def get_pin_user
+        pin = Mailer.find_by(email: params[:email])
+
+        pin.nil? ? "" : pin.pin
+    end
 end
